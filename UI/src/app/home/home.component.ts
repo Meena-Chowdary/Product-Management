9738cb53a9b3ee0485236531ac29d1a5b096b24d
Meena-Chowdary <47032739+Meena-Chowdary@users.noreply.github.com>
2020-05-11 17:00:20 +0530

    UI code uploaded

---
+++ UI/src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  isLoggedIn = false;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = this.authenticationService.isUserLoggedIn();
+    console.log('menu ->' + this.isLoggedIn);
+  }
+
+  handleLogout() {
+    this.authenticationService.logout();
+  }
+
+}
