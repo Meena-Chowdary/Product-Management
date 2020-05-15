628580cd53717f992623fe7d1278c7ef9504ec21
Meena-Chowdary <47032739+Meena-Chowdary@users.noreply.github.com>
2020-05-15 09:04:51 +0530

    Updated

--- Zuul-Api-Gateway/src/main/java/com/cts/security/SpringSecurity.java
+++ Zuul-Api-Gateway/src/main/java/com/cts/security/SpringSecurity.java
@@ -1,6 +1,7 @@
 package com.cts.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,9 +17,15 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter  {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	 http.cors();
+    	http.cors();
         http.csrf().
-        disable();
+        disable().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
 
     }
 
